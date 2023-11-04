3ac8b4e0f0c7e04dbc9f77a7c4e8bc8bd2abf820
habibayasserr <habibahy613@gmail.com>
2023-11-04 10:00:56 +0200

    Student.java

---
+++ Student.java
@@ -0,0 +1,26 @@
+
+public class Student {
+    private String name;
+    private double finalGrade;
+
+    public Student(String name, double finalGrade) {
+        this.name = name;
+        this.finalGrade = finalGrade;
+    }
+
+
+    public String calculateGrade() {
+        if (finalGrade >= 90) {
+            return "A";
+        } else if (finalGrade >= 80) {
+            return "C";
+        } else if (finalGrade >= 70) {
+            return "B";
+        } else if (finalGrade >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+}
